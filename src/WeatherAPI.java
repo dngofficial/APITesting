447c4ad3acd1cc8daf3f14d25a9f9e3ae0c75f3e
BT_1E10_06 <sample@bths.edu>
2022-05-16 13:50:58 -0400

    Initial commit

---
+++ src/WeatherAPI.java
@@ -0,0 +1,15 @@
+
+public class WeatherAPI {
+
+    private WeatherNetworking networker;
+
+    public WeatherAPI() {
+        this.networker = new WeatherNetworking();
+    }
+
+    public void getForecast(String zipCode, int days)
+    {
+        String response = networker.makeAPICallForForecast(zipCode, days);
+        networker.parseForecast(response);
+    }
+}
